09745460c0537c9400820e49ea418e02d590b572
Luiz Picolo <luizpicolo@gmail.com>
2018-06-26 14:16:00 -0400

    Added generated messsages

--- generator.rb
+++ generator.rb
@@ -1,11 +1,17 @@
 require 'fileutils'
 require_relative 'utils'
 
+puts "Generating project: #{ARGV[0]}"
+
 BASE_DIRS = convert_to_array(read_file('generate_sinatra_app/base/dir'))
+
+puts "Generating directories:"
 BASE_DIRS.each do |base_dir|
+  p remove_write_space(base_dir)
   create_diretory ARGV[0] + '/' + base_dir
 end
 
+puts "Generating initial files:"
 create_and_write_file('/controllers/init.rb')
 create_and_write_file('/models/init.rb')
 create_and_write_file('/public/stylesheet/main.scss')
@@ -14,3 +20,5 @@ create_and_write_file('/views/layout.erb', [read_file('generate_sinatra_app/base
 create_and_write_file('/Gemfile', convert_to_array(read_file('generate_sinatra_app/base/gemfile')))
 create_and_write_file('/app.rb', convert_to_array(read_file('generate_sinatra_app/base/app')))
 create_and_write_file('/.ruby-version', ['2.5.1'])
+
+puts "Generate Sucess"
--- utils.rb
+++ utils.rb
@@ -1,5 +1,5 @@
 def create_diretory(dir_name)
-  FileUtils::mkdir_p dir_name.gsub(/\s+/, "") unless File.directory?(dir_name)
+  FileUtils::mkdir_p remove_write_space(dir_name) unless File.directory?(dir_name)
 end
 
 def read_file(file)
@@ -14,4 +14,8 @@ def create_and_write_file(file_name, strings = nil)
   File.open(ARGV[0] + file_name, "w") do |f|
     strings.each {|string| f.write(string)} unless strings.nil?
   end
+end
+
+def remove_write_space(string)
+  string.gsub(/\s+/, "")
 end
