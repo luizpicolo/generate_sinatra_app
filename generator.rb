f1644ca3d17fb1a8715828b2f34614d796662435
Luiz Picolo <luizpicolo@gmail.com>
2018-06-26 20:05:04 -0400

    Change stylesheet to stylesheets and javascript to javascripts

--- generator.rb
+++ generator.rb
@@ -14,7 +14,7 @@ end
 puts "Generating initial files:"
 create_and_write_file('/controllers/init.rb')
 create_and_write_file('/models/init.rb')
-create_and_write_file('/public/stylesheet/main.scss')
+create_and_write_file('/public/stylesheets/main.scss')
 create_and_write_file('/public/javascript/main.js')
 create_and_write_file('/views/layout.erb', [read_file('generate_sinatra_app/base/layout')])
 create_and_write_file('/Gemfile', convert_to_array(read_file('generate_sinatra_app/base/gemfile')))
