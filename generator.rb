5b09b129abbdbda3a76ff9b46fbf1b2a1b3162b9
Luiz Picolo <luizpicolo@gmail.com>
2018-06-26 22:07:36 -0400

    - added content to controller/init; - added new controller home; - Change javascript to javascripts; - added new home view; - added config.ru;

--- generator.rb
+++ generator.rb
@@ -12,13 +12,16 @@ BASE_DIRS.each do |base_dir|
 end
 
 puts "Generating initial files:"
-create_and_write_file('/controllers/init.rb')
+create_and_write_file('/controllers/init.rb', ["require_relative 'home_controller'"])
+create_and_write_file('/controllers/home_controller.rb', [read_file('generate_sinatra_app/base/home_controller')])
 create_and_write_file('/models/init.rb')
 create_and_write_file('/public/stylesheets/main.scss')
-create_and_write_file('/public/javascript/main.js')
+create_and_write_file('/public/javascripts/main.js')
 create_and_write_file('/views/layout.erb', [read_file('generate_sinatra_app/base/layout')])
+create_and_write_file('/views/home.erb', ['Welcome to Home'])
 create_and_write_file('/Gemfile', convert_to_array(read_file('generate_sinatra_app/base/gemfile')))
 create_and_write_file('/app.rb', convert_to_array(read_file('generate_sinatra_app/base/app')))
 create_and_write_file('/.ruby-version', ['2.5.1'])
+create_and_write_file('/config.ru', convert_to_array(read_file('generate_sinatra_app/base/config_ru')))
 
 puts "Generate Sucess"
